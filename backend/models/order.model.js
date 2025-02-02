ede4900fa47d1c44ad235cf7d6e481f3742a05a7
NazSvn <idler90@gmail.com>
2025-02-02 22:33:38 +0800

    feat: Add Order model
    - Created Order schema to store user orders
    - Ensures referential integrity with User and Product models
    - Adds timestamps for tracking order creation and updates

---
+++ backend/models/order.model.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose'
+
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User'
+    },
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: 'Product'
+        },
+        quantity: { type: Number, required: true, min: 1 },
+        image: { type: String, required: true },
+        price: { type: Number, required: true, min: 0 }
+      }
+    ],
+    totalAmmount: { type: Number, required: true, min: 0 },
+    stripeSessionId: { type: String, required: true, unique: true }
+  },
+  {
+    timestamps: true
+  }
+)
+
+const Order = mongoose.model('Order', orderSchema)
+
+export default Order
