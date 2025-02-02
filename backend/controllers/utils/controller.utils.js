b248ae0d19c3a627bbabd8f082772c03b9821635
NazSvn <idler90@gmail.com>
2025-02-02 22:32:01 +0800

    feat: Add helper functions for Stripe coupon creation
    - Implemented createStripeCoupon to generate Stripe discount coupons
    - Added createNewcoupon to create a new coupon for users with a 30-day expiration
    - Enhances discount functionality and rewards system for users

--- backend/controllers/utils/controller.utils.js
+++ backend/controllers/utils/controller.utils.js
@@ -1,4 +1,6 @@
 import { validationResult } from 'express-validator'
+import Coupon from '../../models/coupon.model'
+import { stripe } from '../../lib/stripe'
 // helper functions to validate request
 
 export const validateRequest = (req) => {
@@ -18,3 +20,24 @@ export const sanitizeUser = (user) => ({
   email: user.email,
   role: user.role
 })
+
+export async function createStripeCoupon(discountPercentage) {
+  const coupon = await stripe.coupons.create({
+    percentange_off: discountPercentage,
+    duration: 'once'
+  })
+
+  return coupon.id
+}
+
+export async function createNewcoupon(userId) {
+  const newCoupon = new Coupon({
+    code: 'Gift' + Math.random().toString(36).substring(2, 8).toUpperCase(),
+    discountPercentage: 10,
+    expirationDate: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 days from now
+    userId: userId
+  })
+
+  await newCoupon.save()
+  return newCoupon
+}
