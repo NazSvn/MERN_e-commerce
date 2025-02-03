00fe3d80acfd3a55750691f2582a940c30eec8f2
NazSvn <idler90@gmail.com>
2025-02-03 11:53:00 +0800

    feat: implement analytics controller with sales data
    - Create analytics controller with combined data response
    - Fetch general analytics metrics
    - Add 7-day sales data calculation

---
+++ backend/controllers/analytics/analytics.controller.js
@@ -0,0 +1,20 @@
+import {
+  getAnalyticsData,
+  getDailySalesData
+} from '../utils/controller.utils.js'
+
+export const getAnalytics = async (req, res) => {
+  try {
+    const analyticsData = await getAnalyticsData()
+
+    const endDate = new Date()
+    const startDate = new Date(endDate.getTime() - 7 * 24 * 60 * 60 * 1000) //  7 days ago
+
+    const dailySalesData = await getDailySalesData(startDate, endDate)
+
+    res.status(200).json({ analyticsData, dailySalesData })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ message: 'Server error', error: error.message })
+  }
+}
