2e5ada47aced9fa0a826329f0cf8ee345814f1e0
NazSvn <idler90@gmail.com>
2025-02-05 22:54:33 +0800

    feat: Enhance App routing with authentication and role-based access
    - Implement protected routes for signup and login
    - Add admin dashboard route with role-based access
    - Integrate checkAuthState for persistent authentication
    - Add Loading component during authentication check
    - Use Navigate for redirecting unauthorized access

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,43 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import Navbar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
 import { Toaster } from "react-hot-toast";
+import { useUserStore } from "./stores/useUserStore";
+import { useEffect } from "react";
+import Loading from "./components/Loading";
+import AdminPage from "./pages/AdminPage";
 
 function App() {
+  const { user, checkAuthState, checkAuth } = useUserStore();
+
+  useEffect(() => {
+    checkAuthState();
+  }, [checkAuthState]);
+
+  if (checkAuth) return <Loading />;
+
   return (
     <div className="relative min-h-screen overflow-hidden bg-gray-900 text-white">
       <div className="relative z-50 pt-20">
         <Navbar />
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
-          <Route path="/signup" element={<SignUpPage />}></Route>
-          <Route path="/login" element={<LoginPage />}></Route>
+          <Route
+            path="/signup"
+            element={!user ? <SignUpPage /> : <HomePage />}
+          ></Route>
+          <Route
+            path="/login"
+            element={!user ? <LoginPage /> : <HomePage />}
+          ></Route>
+          <Route
+            path="/admin-dashboard"
+            element={
+              user?.role === "admin" ? <AdminPage /> : <Navigate to={"/"} />
+            }
+          ></Route>
         </Routes>
       </div>
       <Toaster />
