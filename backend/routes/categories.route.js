f5f36e4ec4483f08f3d68d266381aa56d26e32aa
NazSvn <idler90@gmail.com>
2025-02-10 23:44:31 +0800

    fix: Remove duplicate middleware
    - Remove duplicate `adminMiddleware` from GET route
    - Add `authMiddleware` to category routes for authentication

--- backend/routes/categories.route.js
+++ backend/routes/categories.route.js
@@ -4,12 +4,15 @@ import {
   getAllCategories,
   updateCategory
 } from '../controllers/category/category.controller.js'
-import { adminMiddleware, authMiddleware } from '../middleware/auth.middleware.js'
+import {
+  adminMiddleware,
+  authMiddleware
+} from '../middleware/auth.middleware.js'
 
 const router = express.Router()
 
-router.get('/',adminMiddleware, adminMiddleware, getAllCategories)
-router.post('/',authMiddleware, adminMiddleware, createCategory)
+router.get('/', authMiddleware, adminMiddleware, getAllCategories)
+router.post('/', authMiddleware, adminMiddleware, createCategory)
 router.patch('/:slug', authMiddleware, adminMiddleware, updateCategory)
 
 export default router
