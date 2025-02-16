29e7f0fa6a016ce2bad8d0b7c44b594e210bbb2c
NazSvn <idler90@gmail.com>
2025-02-16 10:44:09 +0800

    feat: integrate dynamic stores and add featured products to HomePage
    - Replace static categories with dynamic data from useCategoryStore
    - Conditionally render FeaturedProducts component based on loading state and available products

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,47 +1,21 @@
+import { useEffect } from "react";
 import CategoryItem from "../components/CategoryItem";
-
-const categories = [
-  {
-    href: "/jeans",
-    name: "Jeans",
-    imageUrl: "/products/men-chino-pants-beige.jpg",
-  },
-  {
-    href: "/tshirts",
-    name: "T-Shirts",
-    imageUrl: "/products/men-golf-polo-t-shirt-blue.jpg",
-  },
-  {
-    href: "/women",
-    name: "hoodie",
-    imageUrl: "/products/women-stretch-popover-hoodie-black.jpg",
-  },
-  {
-    href: "/shoes",
-    name: "shoes",
-    imageUrl: "/products/knit-athletic-sneakers-gray.jpg",
-  },
-  {
-    href: "/bags",
-    name: "dakine",
-    imageUrl: "/products/backpack.jpg",
-  },
-  {
-    href: "/misc",
-    name: "plates",
-    imageUrl: "/products/6-piece-white-dinner-plate-set.jpg",
-  },
-  {
-    href: "/jewelry",
-    name: "jewelry",
-    imageUrl: "/products/double-elongated-twist-french-wire-earrings.webp",
-  },
-];
+import { useProductStore } from "../stores/useProductStore";
+import FeaturedProducts from "../components/FeaturedProducts";
+import { useCategoryStore } from "../stores/useCategoryStore";
 
 const HomePage = () => {
+  const { getFeaturedProducts, products, loading } = useProductStore();
+  const { categories, getCategories } = useCategoryStore();
+
+  useEffect(() => {
+    getFeaturedProducts();
+    getCategories();
+  }, [getFeaturedProducts, getCategories]);
+
   return (
     <div className="relative min-h-screen overflow-hidden text-white">
-      <div className="relative z-10 mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8">
+      <div className="relative z-10 mx-auto max-w-7xl px-4 py-10 sm:px-6 lg:px-8">
         <h1 className="mb-4 text-center text-5xl font-bold text-emerald-400 sm:text-6xl">
           Categories
         </h1>
@@ -55,6 +29,9 @@ const HomePage = () => {
           ))}
         </div>
       </div>
+      {!loading && products.length > 0 && (
+        <FeaturedProducts featuredProducts={products} />
+      )}
     </div>
   );
 };
