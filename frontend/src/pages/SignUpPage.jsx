3a72e7309e4c6d4493d1194ca9f64da2622dbf84
NazSvn <idler90@gmail.com>
2025-02-16 10:53:17 +0800

    refactor: SignUpPage component for performance and readability
    - Utilize useCallback and useMemo hooks to optimize functions and dependencies
    - Implement useRef for form input references to avoid unnecessary re-renders

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { ArrowRight, Loader, Lock, Mail, User, UserPlus } from "lucide-react";
 import { Link } from "react-router-dom";
 import FormInput from "../components/FormInput";
 import { useUserStore } from "../stores/useUserStore";
+import handleSimpleKeydown from "../utils/handleSimpleKeydown";
 
 const SignUpPage = () => {
   const [formData, setFormData] = useState({
@@ -21,15 +22,32 @@ const SignUpPage = () => {
 
   const { signup, loading } = useUserStore();
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const confirmPasswordRef = useRef(null);
 
-  const validateForm = () => {
+  const refs = useMemo(
+    () => ({
+      name: nameRef,
+      email: emailRef,
+      password: passwordRef,
+      confirmPassword: confirmPasswordRef,
+    }),
+    [],
+  );
+
+  const emailRegex = useMemo(
+    () => /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/,
+    [],
+  );
+
+  const passwordRegex = useMemo(
+    () => /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/,
+    [],
+  );
+
+  const validateForm = useCallback(() => {
     const newErrors = {
       name: "",
       email: "",
@@ -43,15 +61,11 @@ const SignUpPage = () => {
       newErrors.name = "Name must be at least 3 characters long";
     }
 
-    const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
     if (!formData.email.trim()) {
       newErrors.email = "Email is required";
     } else if (!emailRegex.test(formData.email)) {
       newErrors.email = "Invalid email format";
     }
-
-    const passwordRegex =
-      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/;
 
     if (!formData.password.trim()) {
       newErrors.password = "Password is required";
@@ -66,19 +80,75 @@ const SignUpPage = () => {
     }
     setErrors(newErrors);
     return Object.values(newErrors).every((error) => error === "");
-  };
+  }, [
+    emailRegex,
+    formData.confirmPassword,
+    formData.email,
+    formData.name,
+    formData.password,
+    passwordRegex,
+  ]);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleInputChange = useCallback((e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  }, []);
 
-    if (validateForm()) {
-      signup(formData);
-    }
-  };
+  const handleSubmit = useCallback(
+    async (e) => {
+      e.preventDefault();
+
+      if (validateForm()) {
+        await signup(formData);
+      }
+    },
+    [formData, signup, validateForm],
+  );
+
+  const inputsConfig = useMemo(
+    () => [
+      {
+        label: "Full name",
+        name: "name",
+        type: "text",
+        placeholder: "Your name",
+        icon: User,
+        ref: refs.name,
+      },
+      {
+        label: "Email address",
+        name: "email",
+        type: "email",
+        placeholder: "Your email address",
+        icon: Mail,
+        ref: refs.email,
+      },
+      {
+        label: "Password",
+        name: "password",
+        type: "password",
+        placeholder: "Password",
+        icon: Lock,
+        ref: refs.password,
+      },
+      {
+        label: "Confirm Password",
+        name: "confirmPassword",
+        type: "password",
+        placeholder: "Confirm Password",
+        icon: Lock,
+        ref: refs.confirmPassword,
+      },
+    ],
+    [refs],
+  );
 
   return (
     <>
-      <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="mx-4 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
         <motion.div
           className="sm:mx-auto sm:w-full sm:max-w-md"
           initial={{ opacity: 0, y: -20 }}
@@ -93,57 +163,26 @@ const SignUpPage = () => {
           className="mt-8 sm:mx-auto sm:w-full sm:max-w-md"
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.8, delay: 0.2 }}
+          transition={{ duration: 0.5 }}
         >
           <div className="bg-gray-800 px-4 py-8 shadow sm:rounded-lg sm:px-10">
             <form className="space-y-6" onSubmit={handleSubmit}>
-              <FormInput
-                label={"Full name"}
-                name={"name"}
-                type={"text"}
-                value={formData.name}
-                onChange={handleInputChange}
-                placeholder={"Your name"}
-                icon={User}
-                error={errors.name}
-              />
-
-              <FormInput
-                label={"Email address"}
-                name={"email"}
-                type={"email"}
-                value={formData.email}
-                onChange={handleInputChange}
-                placeholder={"Your email address"}
-                icon={Mail}
-                error={errors.email}
-              />
-
-              <FormInput
-                label={"Password"}
-                name={"password"}
-                type={"password"}
-                value={formData.password}
-                onChange={handleInputChange}
-                placeholder={"Password"}
-                icon={Lock}
-                error={errors.password}
-              />
-
-              <FormInput
-                label={"Confirm Password"}
-                name={"confirmPassword"}
-                type={"password"}
-                value={formData.confirmPassword}
-                onChange={handleInputChange}
-                placeholder={"Confirm Password"}
-                icon={Lock}
-                error={errors.confirmPassword}
-              />
+              {inputsConfig.map((input) => (
+                <FormInput
+                  key={input.name}
+                  {...input}
+                  value={formData[input.name]}
+                  onChange={handleInputChange}
+                  onKeyDown={(e) =>
+                    handleSimpleKeydown(e, setFormData, input.name, input.ref)
+                  }
+                  error={errors[input.name]}
+                />
+              ))}
 
               <button
                 type="submit"
-                className="flex w-full justify-center rounded-md border border-transparent bg-emerald-600 px-4 py-2 text-sm font-medium text-white shadow-sm transition duration-150 ease-in-out hover:bg-emerald-700 focus:ring-2 focus:ring-emerald-500 focus:ring-offset-2 focus:outline-none disabled:opacity-50"
+                className="flex w-full cursor-pointer justify-center rounded-md border border-transparent bg-emerald-600 px-4 py-2 text-sm font-medium text-white shadow-sm transition duration-150 ease-in-out hover:bg-emerald-700 focus:ring-2 focus:ring-emerald-500 focus:ring-offset-2 focus:outline-none disabled:opacity-50"
                 disabled={loading}
               >
                 {loading ? (
