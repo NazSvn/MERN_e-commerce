03b5b5a953aa1835c44433d82c280b74a45ef25e
NazSvn <idler90@gmail.com>
2025-02-16 16:19:11 +0800

    chore: Add production origin to CORS accepted origins
    - Add deployment url to the list of accepted origins in the CORS middleware.

--- backend/middleware/cors.middleware.js
+++ backend/middleware/cors.middleware.js
@@ -4,7 +4,7 @@ const ACCEPTED_ORIGINS = [
   'http://localhost:3000',
   'http://localhost:8080',
   'http://localhost:5173',
-  'http://127.0.0.1:5500'
+  'https://mern-e-commerce-tydf.onrender.com'
 ]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
